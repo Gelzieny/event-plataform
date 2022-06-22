2439a8ec1acf56fb48c2fe0e937a833d5f815d45
Gelzieny <gelzieny@gmail.com>
2022-06-22 08:08:31 -0300

    Criando componentes Header, Sidebar, Lesson e video

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,15 @@
-import { gql, useQuery } from "@apollo/client";
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`;
-
-interface Lesson {
-  id: string;
-  title: string;
-}
+import { Header } from "./components/Header";
+import { Lesson } from "./components/Lesson";
+import { Sidebar } from "./components/Sidebar";
+import { Video } from "./components/Video";
 
 export function App() {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
-
-  console.log(data);
-
   return (
-    <ul>
-      {data?.lessons.map((lesson) => {
-        return <li key={lesson.id}>{lesson.title}</li>;
-      })}
-    </ul>
+    <>
+      <Header />
+      <Lesson />
+      <Sidebar />
+      <Video />
+    </>
   );
 }
