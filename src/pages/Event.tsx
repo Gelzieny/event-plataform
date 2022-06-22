c8edda854941da70610536e86715f49715608daa
Gelzieny <gelzieny@gmail.com>
2022-06-22 08:36:09 -0300

    Importando cores para tailwindcss

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,15 +1,12 @@
 import { Header } from "../components/Header";
-import { Lesson } from "../components/Lesson";
-import { Sidebar } from "../components/Sidebar";
-import { Video } from "../components/Video";
 
 export function Event() {
   return (
     <>
       <Header />
-      <Lesson />
+      {/* <Lesson />
       <Sidebar />
-      <Video />
+      <Video /> */}
     </>
   );
 }
