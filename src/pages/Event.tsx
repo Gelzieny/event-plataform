f942977d7bc7651d13d48a5ec78c65e465f03676
Gelzieny <gelzieny@gmail.com>
2022-06-23 12:08:32 -0300

    Alternado tela de Evento do projeto colocando mensagem para o usuário Clicar em qualquer aula

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -13,7 +13,14 @@ export function Event() {
           <Video lessonSlug={slug} />
         ) : (
           <div className="flex-1">
-            <img src="../assests/plano_fundo.svg" alt="" />
+            <img
+              className="h-[60vh] w-15 ml-96 px-8 mb-5 mt-24"
+              src="https://cdn.pixabay.com/photo/2013/04/01/10/59/warning-98813_1280.png"
+              alt=""
+            />
+            <h1 className="w-full py-79 font-normal flex items-center text-5xl ml-96 text-sm">
+              Click em uma aula para acessar
+            </h1>
           </div>
         )}
         <Sidebar />
