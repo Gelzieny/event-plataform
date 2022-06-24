6b01ed3ac6ef7e6c9a3f3c44c8cb5a1529d6ef8f
Gelzieny <gelzieny@gmail.com>
2022-06-24 10:56:50 -0300

    Criando pagina Subscribe do projeto, criado formulario

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -13,14 +13,11 @@ export function Event() {
           <Video lessonSlug={slug} />
         ) : (
           <div className="flex-1">
-            <img
-              className="h-[60vh] w-15 ml-96 px-8 mb-5 mt-24"
-              src="https://cdn.pixabay.com/photo/2013/04/01/10/59/warning-98813_1280.png"
-              alt=""
-            />
-            <h1 className="w-full py-79 font-normal flex items-center text-5xl ml-96 text-sm">
-              Click em uma aula para acessar
-            </h1>
+            <div className="min-h-screen w-full h-full bg-blur bg-cover bg-no-repeat flex flex-col items-center">
+              <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
+                <div className="max-w-[700px] mt-[35%] mr-[-10px]"></div>
+              </div>
+            </div>
           </div>
         )}
         <Sidebar />
